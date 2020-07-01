e6ad510f3325b63aa62f7e8e12cb73119dec97e9
DeathRealms <justintoolove@gmail.com>
2020-07-01 18:37:02 -0400

    Initial commit

---
+++ src/main/java/me/deathrealms/parsenear/ParseNear.java
@@ -0,0 +1,57 @@
+package me.deathrealms.parsenear;
+
+import com.google.common.primitives.Ints;
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ParseNear extends PlaceholderExpansion {
+
+    @Override
+    public String getAuthor() {
+        return "DeathRealms";
+    }
+
+    @Override
+    public String getName() {
+        return "ParseNear";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "parsenear";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, String identifier) {
+        String[] args = identifier.split("_");
+        Integer radius = Ints.tryParse(args[0]);
+        List<Entity> nearbyEntities = player.getWorld().getEntities();
+
+        if (radius != null) {
+            nearbyEntities = player.getNearbyEntities(radius, radius, radius);
+            args = (String[]) ArrayUtils.remove(args, 0);
+        }
+
+        Player near = (Player) nearbyEntities.stream()
+                .filter(entity -> entity instanceof Player)
+                .filter(entity -> entity != player)
+                .filter(entity -> entity.getWorld() == player.getWorld())
+                .min(Comparator.comparingDouble(entity -> entity.getLocation().distanceSquared(player.getLocation())))
+                .orElse(null);
+
+        if (near == null) return "";
+
+        return PlaceholderAPI.setPlaceholders(near, "%" + String.join("_", args) + "%");
+    }
+}
